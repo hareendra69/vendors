13e99c13726c23493f0ff4d9f7d39213e3d7e8ae
Alex Katebi <alex.katebi@contractor@cengage.com>
2014-12-15 20:06:51 -0500

    f

--- gulpfile.js
+++ gulpfile.js
@@ -5,63 +5,60 @@ var shell = require('shelljs');
 var notify = require('gulp-notify');
 var markdown = require('gulp-markdown');
 
-var sourceRename = {
-    'ui-bootstrap':  'ui-bootstrap'
-};
-
-var sourceObj = {
+var repos = {
     'gulp': 'https://github.com/gulpjs/gulp.git',
     'ionic': 'https://github.com/driftyco/ionic.git',
+    'myapp': 'http://github.com/alex-katebi/myapp.git',
+    'bootstrap': 'https://github.com/twbs/bootstrap.git',
     'shelljs': 'https://github.com/arturadib/shelljs.git',
-    'angular.js': 'https://github.com/angular/angular.js.git',
-    'ui-router': 'https://github.com/angular-ui/ui-router.git',
-    'ui-bootstrap': 'https://github.com/angular-ui/bootstrap.git',
+    'angular': 'https://github.com/angular/angular.js.git',
     'angular-strap': 'https://github.com/mgcrea/angular-strap',
-    'bootstrap': 'https://github.com/twbs/bootstrap.git'
+    'ui-router': 'https://github.com/angular-ui/ui-router.git',
+    'traceur': 'https://github.com/google/traceur-compiler.git',
+    'ui-bootstrap': 'https://github.com/angular-ui/bootstrap.git',
 };
 
-var sources = Object.keys(sourceObj);
+var names = Object.keys(repos);
 
-gulp.task('default', ['sources']);
+gulp.task('default', ['repos']);
 
-gulp.task('sources', sources);
+gulp.task('repos', names);
 
-sources.forEach(function(source) {
-    gulp.task(source, function() {
+names.forEach(function(name) {
+    gulp.task(name, function() {
         if(!shell.which('git'))
         {
             echo('Sorry, this script requires git');
             shell.exit(1);
         }
-        var repo = sourceObj[source];
-        var name = sourceRename[source] || '';
-        shell.mkdir('-p', __dirname+'/sources');
-        shell.cd(__dirname+'/sources');
-        shell.rm('-rf', __dirname+'/sources/'+source);
+        var repo = repos[name];
+        shell.rm('-rf', name);
         shell.exec('git clone '+repo+' '+name, function(code) {
             if(code) {
-                return;
+                return console.error('Failed clone git repo:', repo);
             }
-            var dir = __dirname+'/sources/'+source;
+            var dir = __dirname+'/'+name;
             var npm = dir+'/package.json';
             var bower = dir+'/bower.json';
             shell.cd(dir);
             fs.stat(npm, function(err) {
                 if(err) return;
                 shell.exec('npm install', {silent:true}, function(code, output) {
+                    if(code) return console.log('Failed', code);
                     console.log('npm install ==>>', dir);
                 });
             });
             fs.stat(bower, function(err) {
                 if(err) return;
                 shell.exec('bower install', {silent:true}, function(code, output) {
+                    if(code) return console.log('Failed', code);
                     console.log('bower install ==>>', dir);
                 });
             });
             gulp.src('**/docs/**/*.md')
                 .pipe(markdown())
                 .pipe(gulp.dest('./docs-html'))
-                .pipe(notify('markdown ==>> '+source));
+                .pipe(notify('markdown ==>> '+name));
         });
     }); 
 });
