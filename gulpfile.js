aa13c293a18ef91a86c2389d4261915cdd02c6d2
Alex Katebi <alex.katebi@contractor@cengage.com>
2014-12-23 11:03:15 -0500

    f

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var notify = require('gulp-notify');
 var markdown = require('gulp-markdown');
 
 var repos = {
+    'd3': 'https://github.com/mbostock/d3.git',
     'gulp': 'https://github.com/gulpjs/gulp.git',
     'ionic': 'https://github.com/driftyco/ionic.git',
     'myapp': 'http://github.com/alex-katebi/myapp.git',
@@ -14,15 +15,11 @@ var repos = {
     'angular': 'https://github.com/angular/angular.js.git',
     'angular-strap': 'https://github.com/mgcrea/angular-strap',
     'ui-router': 'https://github.com/angular-ui/ui-router.git',
-    'traceur': 'https://github.com/google/traceur-compiler.git',
-    'ui-bootstrap': 'https://github.com/angular-ui/bootstrap.git',
 };
 
 var names = Object.keys(repos);
 
-gulp.task('default', ['repos']);
-
-gulp.task('repos', names);
+gulp.task('default', names);
 
 names.forEach(function(name) {
     gulp.task(name, function() {
@@ -44,21 +41,25 @@ names.forEach(function(name) {
             fs.stat(npm, function(err) {
                 if(err) return;
                 shell.exec('npm install', {silent:true}, function(code, output) {
-                    if(code) return console.log('Failed', code);
+                    if(code) return;
                     console.log('npm install ==>>', dir);
                 });
             });
             fs.stat(bower, function(err) {
                 if(err) return;
                 shell.exec('bower install', {silent:true}, function(code, output) {
-                    if(code) return console.log('Failed', code);
+                    if(code) return;
                     console.log('bower install ==>>', dir);
                 });
             });
-            gulp.src('**/docs/**/*.md')
+            gulp.src('*.md')
                 .pipe(markdown())
-                .pipe(gulp.dest('markup'))
-                .pipe(notify('markdown ==>> '+name));
+                .pipe(gulp.dest('*.html'))
+                .pipe(notify('*.md to html ==>> '+name));
+            gulp.src('**/docs/**/*.md', {base:'docs'})
+                .pipe(markdown())
+                .pipe(gulp.dest('docs-html'))
+                .pipe(notify('docs to html ==>> '+name));
         });
     }); 
 });
